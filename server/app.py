7c0a0b07943d6e68f7a0b4e0f0d9cd029b6c0727
JohnKaranja <jonniekaras@gmail.com>
2024-07-07 20:06:09 +0300

    Passed all tests and completed tasks

--- server/app.py
+++ server/app.py
@@ -24,6 +24,55 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class RestaurantsResource(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [restaurant.to_dict(only=('address', 'id', 'name')) for restaurant in restaurants], 200
+class RestaurantResource(Resource):
+    def get(self, id):
+        restaurant = db.session.get(Restaurant, id)
+        if restaurant:
+            return restaurant.to_dict(), 200
+        return {"error": "Restaurant not found"}, 404
+
+    def delete(self, id):
+        restaurant = db.session.get(Restaurant, id)
+        if restaurant:
+            RestaurantPizza.query.filter_by(restaurant_id=id).delete()
+            db.session.delete(restaurant)
+            db.session.commit()
+            return '', 204
+        return {"error": "Restaurant not found"}, 404
+    
+class PizzasResource(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [pizza.to_dict(only=('id', 'ingredients', 'name')) for pizza in pizzas], 200
+
+class RestaurantPizzasResource(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            return new_restaurant_pizza.to_dict(), 201
+        except ValueError:
+            db.session.rollback()
+            return {"errors": ["validation errors"]}, 400
+        except Exception as e:
+            db.session.rollback()
+            return {"errors": [str(e)]}, 400
+        
+
+api.add_resource(RestaurantsResource, '/restaurants')
+api.add_resource(RestaurantResource, '/restaurants/<int:id>')
+api.add_resource(PizzasResource, '/pizzas')
+api.add_resource(RestaurantPizzasResource, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
